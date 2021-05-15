1ce5e2c229b8d1b86e58760bbb9304131a103cde
TheRealJan <jan.m.tennert@gmail.com>
2021-05-15 15:54:03 +0200

    Add KEventListener for future updates

--- src/main/kotlin/de/jan/jdaktx/eventmanager/KEventManager.kt
+++ src/main/kotlin/de/jan/jdaktx/eventmanager/KEventManager.kt
@@ -3,18 +3,16 @@ package de.jan.jdaktx.eventmanager
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.events.Event
 import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.hooks.EventListener
 import net.dv8tion.jda.api.hooks.IEventManager
 
 class KEventManager : IEventManager {
 
-    @PublishedApi
-    internal val listeners = mutableListOf<EventListener>()
+    private val listeners = mutableListOf<Any>()
 
     override fun register(e: Any) {
-        if (e is EventListener) {
+        if (e is EventListener || e is KEventListener) {
             listeners.add(e)
         } else {
             throw UnsupportedOperationException()
@@ -22,7 +20,7 @@ class KEventManager : IEventManager {
     }
 
     override fun unregister(e: Any) {
-        if (e is EventListener) {
+        if (e is EventListener || e is KEventListener) {
             listeners.remove(e)
         } else {
             throw UnsupportedOperationException()
@@ -30,24 +28,29 @@ class KEventManager : IEventManager {
     }
 
     override fun handle(e: GenericEvent) {
-        val iterator = listeners.iterator()
-        while (iterator.hasNext()) {
-            iterator.next().onEvent(e)
+        GlobalScope.launch {
+            val iterator = listeners.iterator()
+            while (iterator.hasNext()) {
+                when (val next = iterator.next()) {
+                    is KEventListener -> next.onEvent(e)
+                    is EventListener -> next.onEvent(e)
+                }
+            }
         }
     }
 
-    override fun getRegisteredListeners(): MutableList<EventListener> {
+    override fun getRegisteredListeners(): MutableList<Any> {
         return listeners
     }
 
 }
 
-inline fun <reified T : Event> JDA.on(crossinline event: suspend (T) -> Unit) {
+inline fun <reified T : GenericEvent> JDA.on(crossinline event: suspend (T) -> Unit) {
     if (!this.hasKotlinExtensions) setupKotlinExtensions()
-    val listener = EventListener {
-        if (it is T) {
-            GlobalScope.launch {
-                event.invoke(it)
+    val listener = object : KEventListener {
+        override suspend fun onEvent(e: GenericEvent) {
+            if (e is T) {
+                event.invoke(e)
             }
         }
     }
@@ -56,6 +59,10 @@ inline fun <reified T : Event> JDA.on(crossinline event: suspend (T) -> Unit) {
 
 fun JDA.setupKotlinExtensions() {
     this.setEventManager(KEventManager())
+}
+
+interface KEventListener {
+    suspend fun onEvent(e: GenericEvent)
 }
 
 val JDA.hasKotlinExtensions: Boolean
