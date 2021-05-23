9a6d9c240b5b126090a7f8dc092ffa5e9ad98d71
TheRealJan <jan.m.tennert@gmail.com>
2021-05-23 03:07:03 +0200

    Add DiscordTogether.kt features

---
+++ src/main/kotlin/de/jan/jdaktx/utils/DiscordTogether.kt
@@ -0,0 +1,11 @@
+package de.jan.jdaktx.utils
+
+object DiscordTogether {
+
+    const val YOUTUBE = 755600276941176913
+    const val POKER = 755827207812677713
+    const val BETRAYAL = 773336526917861400
+    const val FISHING = 814288819477020702
+    const val CHESS = 832012586023256104
+
+}
